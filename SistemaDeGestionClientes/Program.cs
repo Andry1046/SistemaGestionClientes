c882959e12a6ae554d3eba17d1d9c9faf811498a
Andry Martinez <andry46@outlook.es>
2025-04-07 19:47:23 -0500

    Feat: Initial Structure Proyect

---
+++ SistemaDeGestionClientes/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaDeGestionClientes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahora mismo esto es distinto");
+
+        }
+    }
+}
